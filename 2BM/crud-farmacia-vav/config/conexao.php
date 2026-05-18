e708151eb324b1d03bc37734a6cd83a75c4b839e
Pedro Henrique Miranda <219332381+M1randaPHM@users.noreply.github.com>
2026-05-18 15:48:19 -0300

    arrumando erros na conexao pdo

--- 2BM/crud-farmacia-vav/config/conexao.php
+++ 2BM/crud-farmacia-vav/config/conexao.php
@@ -1,1 +1,19 @@
-<?php`r`n$host = "localhost";`r`n$db = "farmacia";`r`n$user = "root";`r`n$pass = "";`r`n`r`n$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+<?php
+$host = "localhost";
+$db   = "farmacia";
+$user = "root";
+$pass = "";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
