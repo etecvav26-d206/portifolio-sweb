6ccb987285c5d7c9503342207463f460c1477573
Pedro Henrique Miranda <219332381+M1randaPHM@users.noreply.github.com>
2026-05-18 15:26:42 -0300

    fazendo a conexao com banco

--- 2BM/crud-farmacia-vav/config/conexao.php
+++ 2BM/crud-farmacia-vav/config/conexao.php
@@ -1,1 +1,1 @@
-<?php // placeholder vazio - preencha com credenciais quando necessário
+<?php`r`n$host = "localhost";`r`n$db = "farmacia";`r`n$user = "root";`r`n$pass = "";`r`n`r`n$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
