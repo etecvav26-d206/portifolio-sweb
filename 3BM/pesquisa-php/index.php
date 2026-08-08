fa8e1907edcd74f1449baa78edcbdad4aa5b5dd6
Pedro Henrique Dalle Molle Godoi <105134628+phznorte777@users.noreply.github.com>
2026-08-08 19:25:00 -0300

    adicionando sha1 no php

--- 3BM/pesquisa-php/index.php
+++ 3BM/pesquisa-php/index.php
@@ -31,7 +31,7 @@ $iv    = "1234567890123456";
                 <thead><tr><th>Funcao</th><th>Tipo</th><th>Descricao</th><th>Resultado</th></tr></thead>
                 <tbody>
                     <tr><td><code>md5()</code></td><td>Hash MD5</td><td>Hash de 128 bits (32 hex).</td><td><code><?php echo md5($texto); ?></code></td></tr>
-                    <tr><td><code>sha1()</code></td><td>Hash SHA1</td><td>Hash de 160 bits (40 hex).</td><td><code>-</code></td></tr>
+                    <tr><td><code>sha1()</code></td><td>Hash SHA1</td><td>Hash de 160 bits (40 hex).</td><td><code><?php echo sha1($texto); ?></code></td></tr>
                     <tr><td><code>hash('sha256')</code></td><td>Hash SHA256</td><td>Hash de 256 bits.</td><td><code>-</code></td></tr>
                     <tr><td><code>password_hash()</code></td><td>Senha Segura</td><td>Hash bcrypt para senhas.</td><td><code>-</code></td></tr>
                     <tr><td><code>base64_encode()</code></td><td>Base64</td><td>Codificacao Base64.</td><td><code>-</code></td></tr>
